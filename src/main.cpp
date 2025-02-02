b7eb9ed00bddaa90aac3f8b57972cd8a8cff334d
Liban Ansari <liban.ansari05@gmail.com>
2025-02-02 23:43:04 +0530

    The type builtin: builtins

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,19 @@ int main() {
 
     string firstWord = input.substr(0, input.find(' ')); 
 
-    if(firstWord == "echo"){
+    if(firstWord == "type"){
+      string to_check = input.substr(5);
+      if(to_check == "echo" || to_check == "exit" || to_check == "type"){
+        cout << to_check << " is a shell builtin" << endl;
+      }else{
+        cout << to_check << ": not found" << endl;
+      }
+    }else if(firstWord == "echo"){
       cout << input.substr(input.find(' ')+1) << endl;
     }else{
       cout << input << ": command not found" << endl;
     }
-
+    
   }
 
   return 0;
