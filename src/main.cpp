a624b95ca448a5477329ce11f70d5688fecf94ff
Liban Ansari <liban.ansari05@gmail.com>
2025-02-03 20:46:59 +0530

    Run a Program

--- src/main.cpp
+++ src/main.cpp
@@ -3,102 +3,99 @@
 using namespace std;
 namespace fs = filesystem;
 
-vector<string> split_string(const string &s, char delimiter){
+vector<string> split_string(const string &s, char delimiter) {
   stringstream ss(s);
   vector<string> return_vect;
   string token;
-  while(getline(ss, token, delimiter)){
+  while (getline(ss, token, delimiter)) {
     return_vect.push_back(token);
   }
   return return_vect;
 }
 
-string get_path(string command){
-  // Get the PATH environment variable
+string get_path(string command) {
+  if (command.find('/') != string::npos) {
+    if (fs::exists(command) && fs::is_regular_file(command)) {
+      return fs::absolute(command).string();
+    } else {
+      return "";
+    }
+  }
+
   string path_env = getenv("PATH");
   stringstream ss(path_env);
-
   string path;
-  // Split the PATH variable by ':' and check each directory
-  while(getline(ss, path, ':')){
-      string abs_path = path + "/" + command;
 
-      // Check if the command exists in the current directory
-      if(fs::exists(abs_path)){
-          return abs_path;
-      }
+  while (getline(ss, path, ':')) {
+    string abs_path = path + "/" + command;
+    if (fs::exists(abs_path) && fs::is_regular_file(abs_path)) {
+      return abs_path;
+    }
   }
   return "";
 }
 
-int main(){
+int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while(true){
+  while (true) {
     cout << "$ ";
     string input;
     getline(cin, input);
 
-    if(input == "exit 0") return 0;
+    if (input == "exit 0") return 0;
 
     string path_env = getenv("PATH");
     vector<string> path = split_string(path_env, ':');
-    
+
     istringstream iss(input);
     vector<string> tokens;
     string token;
-    while(iss >> token) tokens.push_back(token);
-    
-    if(tokens.empty()) continue;
-    
+    while (iss >> token)
+      tokens.push_back(token);
+
+    if (tokens.empty()) continue;
+
     string command = tokens[0];
-    
-    if(command == "type"){
-      if(tokens.size() < 2) continue;
+
+    if (command == "type") {
+      if (tokens.size() < 2)
+        continue;
       string cmd = tokens[1];
-      if(cmd == "echo" or cmd == "exit" or cmd == "type"){
+      if (cmd == "echo" || cmd == "exit" || cmd == "type") {
         cout << cmd << " is a shell builtin" << endl;
-      }else{
+      } else {
         string path = get_path(cmd);
-        if(!path.empty()){
+        if (!path.empty()) {
           cout << cmd << " is " << path << endl;
-        }else{
+        } else {
           cout << cmd << ": not found" << endl;
         }
       }
-    }else if(command == "echo"){
+    } else if (command == "echo") {
       cout << input.substr(input.find(' ') + 1) << endl;
-    }else{
-      // string filepath;
-      // for(int i = 0; i < path.size(); i++){
-      //   filepath = path[i] + '/' + tokens[0];
-      //   ifstream file(filepath);
-      //   if(file.good()){
-      //     string command = "exec " + path[i] + '/' + input;
-      //     system(command.c_str());
-      //     break;
-      //   }else if(i == path.size() - 1){
-      //     cout << tokens[0] << ": not found" << endl;
-      //   }
-      string cmd = input.substr(0, input.find(' '));
+    } else {
+      string cmd = tokens[0];
       string path = get_path(cmd);
-      if(path.empty()){
-        cout << input << ": command not found" << endl;
-      }else{
-        string args = input.substr(cmd.length());
-        string full = path + args; 
-        char buffer[128];
-        string result;
-
-        FILE *pipe = popen(full.c_str(), "r");
-
-        while(!feof(pipe)){
-          if(fgets(buffer, 128, pipe) != NULL){
-            cout << buffer;
-          }
+      if (path.empty()) {
+        cout << cmd << ": command not found" << endl;
+      } else {
+        FILE *pipe = popen(input.c_str(), "r");
+        if (!pipe) {
+          cerr << "Error executing command" << endl;
+          continue;
         }
-        pclose(pipe);
+
+        char buffer[128];
+        while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+          cout << buffer;
+        }
+
+        int status = pclose(pipe);
+        if (status != 0) {
+          cerr << "Command exited with status " << status << endl;
+        }
       }
     }
   }
